ecc03ecf23b89eb55a4cb5549f276b500b2014cf
Feldrise <admin@feldrise.com>
2023-11-18 16:23:55 +0100

    🚀 Publish on Android

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,7 +1,7 @@
 // Local
-const String kApiRootURL = "http://localhost:8080";
-const String kApiBaseURL = "http://localhost:8080/api/v1";
+// const String kApiRootURL = "http://localhost:8080";
+// const String kApiBaseURL = "http://localhost:8080/api/v1";
 
-// // Production
-// const String kApiRootURL = "https://api.masoiree.feldrise.com";
-// const String kApiBaseURL = "https://api.masoiree.feldrise.com/api/v1";
+// Production
+const String kApiRootURL = "https://api.jdl.feldrise.com";
+const String kApiBaseURL = "https://api.jdl.feldrise.com/api/v1";
